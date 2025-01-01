1c9bf54fcc8335bec89aa3e85850ca41e33fb0e1
Muzammal-Rafique <muzammalrafique26@gmail.com>
2025-01-01 18:41:13 +0500

    Fix the upvote Issue

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,6 +1,6 @@
-import { Schema, model, models, Document } from "mongoose";
+import { Schema, models, model, Document } from 'mongoose';
 
-export interface IQuestions extends Document {
+export interface IQuestion extends Document {
   title: string;
   content: string;
   tags: Schema.Types.ObjectId[];
@@ -15,15 +15,15 @@ export interface IQuestions extends Document {
 const QuestionSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
   views: { type: Number, default: 0 },
-  upvotes: { type: Schema.Types.ObjectId, ref: "User" },
-  downvotes: { type: Schema.Types.ObjectId, ref: "User" },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
-  createdAt: { type: Date, default: Date.now },
-});
+  upvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }],
+  createdAt: { type: Date, default: Date.now }
+})
 
-const Question = models.Question || model("Question", QuestionSchema);
+const Question = models.Question || model('Question', QuestionSchema);
 
 export default Question;
