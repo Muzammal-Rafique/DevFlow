c201d60363fb2575a37b1b1bfd3117e4eaa60f8d
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-01-12 11:48:33 +0500

    Adding toast

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,26 @@
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Navbar from "@/components/shared/navbar/Navbar";
-import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+import React from 'react'
+import Navbar from '@/components/shared/navbar/Navbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import { Toaster } from '@/components/ui/toaster'
+const Layout = ({children} : {children : React.ReactNode}) => {
   return (
-    <main className="background-light850_dark100 relative">
-      <Navbar />
-      <div className="flex">
-        <LeftSidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-          <div className="mx-auto w-full">{children}</div>
-        </section>
-        <RightSidebar />
-      </div>
-      Toastar
-    </main>
-  );
-};
+    <main className='background-light850_dark100 relative'>
+        <Navbar />
+        <div className='flex'>
+            <LeftSidebar /> 
+            <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
+                <div className='mx-auto w-full max-w-5xl'>
+                    {children}
+                </div>
+            </section>
+            <RightSidebar />
+        </div>
 
-export default Layout;
+        <Toaster />
+    </main>
+  )
+}
+
+export default Layout
